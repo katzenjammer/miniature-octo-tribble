f340805178ac69608f4790c3789be04a00b2962a
Hal Phillips <softwareevaluator@yahoo.com>
2013-11-09 11:49:04 -0800

    Test commit

---
+++ curses_rocket/curses_rocket/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  curses_rocket
+//
+//  Created by Hal Phillips on 11/9/13.
+//  Copyright (c) 2013 Hal Phillips. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
